1308f95fcdccc265d7784555ba6e274d82d87a68
javacs3 <javacs3@gmail.com>
2020-03-07 16:38:30 +0800

    fix docs build error

--- demo/index.ts
+++ demo/index.ts
@@ -1,10 +1,10 @@
-import { CastPlayer } from '../src/Player'
+import { XtermPlayer } from '../src/Player'
 import cast1 from '../assets/1.cast'
 import cast5 from '../assets/5.cast'
 
 const div = document.getElementById('app')
 if (div) {
-  const player = new CastPlayer(cast1, div)
+  const player = new XtermPlayer(cast1, div)
   // setTimeout(() => {
   //   player.url = cast5
   // }, 5000)
