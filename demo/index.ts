ef8b497b5d0cba7a02829383b5a7d3d2cc581128
javacs3 <javacs3@gmail.com>
2020-03-07 22:41:51 +0800

    add sample cast file selection for demo

--- demo/index.ts
+++ demo/index.ts
@@ -2,11 +2,18 @@ import { XtermPlayer } from '../src/Player'
 import cast1 from '../assets/1.cast'
 import cast5 from '../assets/5.cast'
 
-const div = document.getElementById('app')
-if (div) {
-  const player = new XtermPlayer(cast1, div)
-  // setTimeout(() => {
-  //   player.url = cast5
-  // }, 5000)
+function $id(id: string): HTMLElement {
+  const el = document.getElementById(id)
+  if (!el) { throw new Error('Cannot find element ' + id) }
+  return el
 }
 
+const app = $id('app')
+const castOption = <HTMLSelectElement>$id('cast-option')
+
+const player = new XtermPlayer(cast1, app)
+
+castOption.onchange = () => {
+  console.log('select', castOption.value)
+  player.url = castOption.value
+}
