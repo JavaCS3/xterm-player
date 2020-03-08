d6dca1d0139b52b99f90f459e6734755b189395e
javacs3 <javacs3@gmail.com>
2020-03-08 13:22:48 +0800

    refine demo page

--- demo/index.ts
+++ demo/index.ts
@@ -1,11 +1,17 @@
 import { XtermPlayer } from '../src/Player'
 import cast1 from '../assets/1.cast'
-import cast5 from '../assets/5.cast'
 
 function $id(id: string): HTMLElement {
   const el = document.getElementById(id)
   if (!el) { throw new Error('Cannot find element ' + id) }
   return el
+}
+
+const SAMPLE_CAST_URLS: { [key: string]: string } = {
+  'asciinema-1': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/1.cast',
+  'asciinema-2': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/5.cast',
+  'asciinema-3': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/4.cast',
+  'terminalizer': 'https://raw.githubusercontent.com/faressoft/terminalizer-player/master/data.json'
 }
 
 const app = $id('app')
@@ -14,5 +20,5 @@ const castOption = <HTMLSelectElement>$id('cast-option')
 const player = new XtermPlayer(cast1, app)
 
 castOption.onchange = () => {
-  player.url = castOption.value
+  player.url = SAMPLE_CAST_URLS[castOption.value] || cast1
 }
