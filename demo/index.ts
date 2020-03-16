f17f5cd72d51b7a16521921adf6c4d27804a5b02
javacs3 <javacs3@gmail.com>
2020-03-16 21:37:54 +0800

    update demo page
    1. add a audio supported cast

--- demo/index.ts
+++ demo/index.ts
@@ -1,5 +1,6 @@
 import { XtermPlayer } from '../src/Player'
-import cast1 from '../assets/1.cast'
+
+const audioCast = require('../assets/audio.cast')
 
 function $id(id: string): HTMLElement {
   const el = document.getElementById(id)
@@ -8,17 +9,18 @@ function $id(id: string): HTMLElement {
 }
 
 const SAMPLE_CAST_URLS: { [key: string]: string } = {
+  'sample cast with audio': audioCast,
   'asciinema-1': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/1.cast',
   'asciinema-2': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/5.cast',
   'asciinema-3': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/4.cast',
-  'terminalizer': 'https://raw.githubusercontent.com/faressoft/terminalizer-player/master/data.json'
+  'terminalizer': 'https://raw.githubusercontent.com/faressoft/terminalizer-player/master/data.json',
 }
 
 const app = $id('app')
 const castOption = <HTMLSelectElement>$id('cast-option')
 
-const player = new XtermPlayer(cast1, app)
+const player = new XtermPlayer(audioCast, app)
 
 castOption.onchange = () => {
-  player.url = SAMPLE_CAST_URLS[castOption.value] || cast1
+  player.url = SAMPLE_CAST_URLS[castOption.value] || audioCast
 }
