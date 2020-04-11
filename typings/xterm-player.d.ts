c58a8cc0bc4f40bf980e0e261b04742406444826
javacs3 <javacs3@gmail.com>
2020-04-11 12:41:08 +0800

    add dot at each doc's line end

--- typings/xterm-player.d.ts
+++ typings/xterm-player.d.ts
@@ -6,45 +6,45 @@ declare module 'xterm-player' {
   import * as xterm from 'xterm'
 
   /**
-   * An object containing start up options for the player
+   * An object containing start up options for the player.
    */
   export interface IPlayerOptions {
     /**
-     * The font size used to render text
+     * The font size used to render text.
      */
     fontSize?: number
 
     /**
-     * The font family used to render text
+     * The font family used to render text.
      */
     fontFamily?: string
 
     /**
-     * The font weight used to render non-bold text
+     * The font weight used to render non-bold text.
      */
     fontWeight?: xterm.FontWeight
 
     /**
-     * The font weight used to render bold text
+     * The font weight used to render bold text.
      */
     fontWeightBold?: xterm.FontWeight
 
     /**
-     * The color theme of the terminal
+     * The color theme of the terminal.
      */
     theme?: xterm.ITheme
   }
 
   /**
-   * An object that can be disposed via a dispose function.
+   * An object that can be disposed via a dispose function..
    */
   export interface IDisposable {
     dispose(): void
   }
 
   /**
-   * An event that can be listened to.
-   * @returns an `IDisposable` to stop listening.
+   * An event that can be listened to..
+   * @returns an `IDisposable` to stop listening..
    */
   export interface IEvent<T> {
     (listener: (arg: T) => void): IDisposable
@@ -54,105 +54,105 @@ declare module 'xterm-player' {
 
   export class XtermPlayer {
     /**
-     * The element containing the player
+     * The element containing the player.
      */
     readonly el: HTMLElement
 
     /**
-     * Gets or sets the address or URL of the cast
+     * Gets or sets the address or URL of the cast.
      */
     url: string
 
     /**
-     * Gets or sets the current rate of speed for the cast to play
+     * Gets or sets the current rate of speed for the cast to play.
      */
     playbackRate: number
 
     /**
-     * Gets or sets the current playback position, in milliseconds
+     * Gets or sets the current playback position, in milliseconds.
      */
     currentTime: number
 
     /**
-     * Gets or sets the volume level if the cast has audio
+     * Gets or sets the volume level if the cast has audio.
      */
     volume: number
 
     /**
-     * Gets or sets a flag that indicates whether the audio is muted
+     * Gets or sets a flag that indicates whether the audio is muted.
      */
     muted: boolean
 
     /**
-     * Returns the duration in milliseconds of the current cast. A NaN value is returned if duration is not available, or Infinity if the cast is streaming.
+     * Returns the duration in milliseconds of the current cast. A NaN value is returned if duration is not available, or Infinity if the cast is streaming..
      */
     readonly duration: number
 
     /**
-     * The state of the player
+     * The state of the player.
      */
     readonly state: IPlayerState
 
     /**
-     * Create the player object
-     * @param url The url of xterm video cast file
-     * @param el The element to create the the player within
-     * @param options The options for the player
+     * Create the player object.
+     * @param url The url of xterm video cast file.
+     * @param el The element to create the the player within.
+     * @param options The options for the player.
      */
     constructor(url: string, el: HTMLElement, options?: IPlayerOptions)
 
     /**
-    * Adds an event listener for when the player is ready to play
+    * Adds an event listener for when the player is ready to play.
     */
     readonly onReady: IEvent<void>
 
     /**
-     * Adds an event listener for when the player is loading
+     * Adds an event listener for when the player is loading.
      */
     readonly onLoading: IEvent<void>
 
     /**
-     * Adds an event listener for when the player finished current render
+     * Adds an event listener for when the player finished current render.
      */
     readonly onAfterRender: IEvent<void>
 
     /**
-     * Adds an event listener for when the player currentTime changed
+     * Adds an event listener for when the player currentTime changed.
      */
     readonly onCurrentTimeChanged: IEvent<number>
 
     /**
-     * Adds an event listener for when the player playbackRate changed
+     * Adds an event listener for when the player playbackRate changed.
      */
     readonly onPlaybackRateChanged: IEvent<number>
 
     /**
-     * Adds an event listener for when the player volume changed
+     * Adds an event listener for when the player volume changed.
      */
     readonly onVolumeChanged: IEvent<number>
 
     /**
-     * Adds an event listener for when the player muted changed
+     * Adds an event listener for when the player muted changed.
      */
     readonly onMuteChanged: IEvent<boolean>
 
     /**
-     * Adds an event listener for when the player state changed
+     * Adds an event listener for when the player state changed.
      */
     readonly onStateChanged: IEvent<IPlayerState>
 
     /**
-     * Starts the cast playback
+     * Starts the cast playback.
      */
     play(): void
 
     /**
-     * Replay the cast playback
+     * Replay the cast playback.
      */
     replay(): void
 
     /**
-     * Pause the cast playback
+     * Pause the cast playback.
      */
     pause(): void
   }
