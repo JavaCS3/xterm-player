32cec4ff82c6ef73bfe215bf0989617474327f2f
javacs3 <javacs3@gmail.com>
2020-04-11 23:01:57 +0800

    add theme select in demo page

--- demo/index.ts
+++ demo/index.ts
@@ -1,6 +1,7 @@
 import { XtermPlayer } from '../src/Player'
+const AUDIO_CAST = require('../assets/audio.cast')
 
-const audioCast = require('../assets/audio.cast')
+type BuiltinTheme = 'THEME_SOLARIZED_DARK' | 'THEME_SOLARIZED_LIGHT'
 
 function $id(id: string): HTMLElement {
   const el = document.getElementById(id)
@@ -9,7 +10,7 @@ function $id(id: string): HTMLElement {
 }
 
 const SAMPLE_CAST_URLS: { [key: string]: string } = {
-  'sample cast with audio': audioCast,
+  'sample cast with audio': AUDIO_CAST,
   'asciinema-1': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/1.cast',
   'asciinema-2': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/5.cast',
   'asciinema-3': 'https://raw.githubusercontent.com/JavaCS3/xterm-player/master/assets/4.cast',
@@ -18,9 +19,13 @@ const SAMPLE_CAST_URLS: { [key: string]: string } = {
 
 const app = $id('app')
 const castOption = <HTMLSelectElement>$id('cast-option')
+const themeOption = <HTMLSelectElement>$id('theme-option')
 
-const player = new XtermPlayer(audioCast, app)
+const player = new XtermPlayer(AUDIO_CAST, app)
 
 castOption.onchange = () => {
-  player.url = SAMPLE_CAST_URLS[castOption.value] || audioCast
+  player.url = SAMPLE_CAST_URLS[castOption.value] || AUDIO_CAST
+}
+themeOption.onchange = () => {
+  player.options = { theme: XtermPlayer[<BuiltinTheme>themeOption.value] }
 }
